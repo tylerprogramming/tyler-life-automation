89c2ed71ea44011def2b68300ef02129343c1d8e
tylerreed <tylerreedytlearning@gmail.com>
2025-06-09 06:23:14 -0400

    added more helper functions and updates to the authorization for social media

--- services/instagram.py
+++ services/instagram.py
@@ -7,17 +7,15 @@ from models.youtube import YouTubeTranscriptionCreate
 
 load_dotenv()
 
+INSTAGRAM_ACCOUNT_ID = os.getenv("INSTAGRAM_ACCOUNT_ID")
+INSTAGRAM_ACCESS_TOKEN = os.getenv("INSTAGRAM_ACCESS_TOKEN")
+
 def refresh_instagram_access_token():
-    ACCESS_TOKEN = os.getenv("INSTAGRAM_ACCESS_TOKEN")
-    print("ACCESS_TOKEN:", ACCESS_TOKEN)
-    url = f"https://graph.instagram.com/refresh_access_token?grant_type=ig_refresh_token&access_token={ACCESS_TOKEN}"
+    url = f"https://graph.instagram.com/refresh_access_token?grant_type=ig_refresh_token&access_token={INSTAGRAM_ACCESS_TOKEN}"
     response = requests.get(url)
     print(response.json())
 
 async def post_to_instagram(youtube_record: YouTubeTranscriptionCreate):
-    ACCESS_TOKEN = os.getenv("INSTAGRAM_ACCESS_TOKEN")
-    INSTAGRAM_ACCOUNT_ID = os.getenv("INSTAGRAM_ACCOUNT_ID")
-    
     youtube_video_url = f"https://www.youtube.com/watch?v={youtube_record.video_id}"
     
     input_data = f"Transcription: {youtube_record.transcription} Video URL: {youtube_video_url}"
@@ -38,7 +36,7 @@ async def post_to_instagram(youtube_record: YouTubeTranscriptionCreate):
     payload = {
         "image_url": "test_url",
         "caption": "test_caption",
-        "access_token": ACCESS_TOKEN
+        "access_token": INSTAGRAM_ACCESS_TOKEN
     }
     response = requests.post(url, data=payload)
     media_id = response.json().get("id")
@@ -47,47 +45,54 @@ async def post_to_instagram(youtube_record: YouTubeTranscriptionCreate):
 
     # Publish the uploaded media
     publish_url = f"https://graph.instagram.com/v22.0/{INSTAGRAM_ACCOUNT_ID}/media_publish"
-    publish_response = requests.post(publish_url, data={"creation_id": media_id, "access_token": ACCESS_TOKEN})
+    publish_response = requests.post(publish_url, data={"creation_id": media_id, "access_token": INSTAGRAM_ACCESS_TOKEN})
 
     print("Publish Response:", publish_response.json())
 
-async def post_instagram_image(image_url = None):
-    ACCESS_TOKEN = os.getenv("INSTAGRAM_ACCESS_TOKEN")
-    INSTAGRAM_ACCOUNT_ID = os.getenv("INSTAGRAM_ACCOUNT_ID")
-    
+async def post_instagram_image(image_urls: list[str] = None):
     youtube_record = database_service.check_latest_transcription()
     youtube_video_url = f"https://www.youtube.com/watch?v={youtube_record.video_id}"
     
     input_data = f"Transcription: {youtube_record.transcription} Video URL: {youtube_video_url}"
     
-    print("Youtube Record:", youtube_record)
-    
     if youtube_record:
         agent_output = await instagram_agent_runner(input_data)
-        
-        print("Agent Output:", agent_output)
-        
-        database_service.save_instagram_post(youtube_record.id, agent_output.caption, image_url)
-        
-        # Upload Image to Instagram
-        url = f"https://graph.instagram.com/v22.0/{INSTAGRAM_ACCOUNT_ID}/media"
-        payload = {
-            "image_url": image_url,
-            "caption": agent_output.caption,
-            "access_token": ACCESS_TOKEN
-        }
-        response = requests.post(url, data=payload)
-        media_id = response.json().get("id")
 
-        print("Media ID:", media_id)
-
-        # Publish the uploaded media
-        publish_url = f"https://graph.instagram.com/v22.0/{INSTAGRAM_ACCOUNT_ID}/media_publish"
-        publish_response = requests.post(publish_url, data={"creation_id": media_id, "access_token": ACCESS_TOKEN})
-
-        print("Publish Response:", publish_response.json())
+        database_service.save_instagram_post(youtube_record.id, agent_output.caption, ",".join(image_urls))
+        print("Image URLs:", image_urls)
+        if len(image_urls) > 1:
+            children_ids = []
+            for image_url in image_urls:
+                child_id = upload_image_only_to_instagram(image_url)
+                print("Child ID:", child_id)
+                children_ids.append(child_id['id'])
+                
+            container_id = upload_carousel_to_instagram(agent_output.caption, children_ids)
+            print("Container ID:", container_id)
+            publish_media_to_instagram(container_id['id'])
+        else:
+            print("Single Image")
+            # upload_single_image_caption_to_instagram(image_urls[0], agent_output.caption)
         
-        return agent_output.caption
+    #     # Upload Image to Instagram
+    #     url = f"https://graph.instagram.com/v22.0/{INSTAGRAM_ACCOUNT_ID}/media"
+    #     payload = {
+    #         "image_url": image_url,
+    #         "caption": agent_output.caption,
+    #         "access_token": ACCESS_TOKEN
+    #     }
+    #     response = requests.post(url, data=payload)
+    #     media_id = response.json().get("id")
+
+    #     print("Media ID:", media_id)
+
+    #     # Publish the uploaded media
+    #     publish_url = f"https://graph.instagram.com/v22.0/{INSTAGRAM_ACCOUNT_ID}/media_publish"
+    #     publish_response = requests.post(publish_url, data={"creation_id": media_id, "access_token": ACCESS_TOKEN})
+
+    #     print("Publish Response:", publish_response.json())
+        
+    #     return agent_output.caption
     else:
         return "No transcription found"
 
@@ -109,6 +114,72 @@ async def post_instagram_image(image_url = None):
 
     # print("Publish Response:", publish_response.json())
 
+def upload_image_only_to_instagram(image_url):
+    """
+    Uploads an image to Instagram using the Graph API with a JSON payload and Bearer token.
+    Equivalent to the provided curl command.
+    """
+    url = f"https://graph.instagram.com/v22.0/{INSTAGRAM_ACCOUNT_ID}/media"
+    headers = {
+        "Content-Type": "application/json",
+        "Authorization": f"Bearer {INSTAGRAM_ACCESS_TOKEN}"
+    }
+    payload = {
+        "image_url": image_url
+    }
+    response = requests.post(url, headers=headers, json=payload)
+    print("Upload Response:", response.json())
+    return response.json()
+
+def upload_single_image_caption_to_instagram(image_url, caption):
+    # Uploads Image to Instagram with caption, doesn't post just yet.  We get a media id.
+    url = f"https://graph.instagram.com/v22.0/{INSTAGRAM_ACCOUNT_ID}/media"
+    
+    payload = {
+        "image_url": image_url,
+        "caption": caption,
+        "access_token": INSTAGRAM_ACCESS_TOKEN
+    }
+    
+    response = requests.post(url, data=payload)
+    media_id = response.json().get("id")
+    
+    print("Response:", response.json())
+
+    print("Media ID:", media_id)
+
+def upload_carousel_to_instagram(caption, children_ids):
+    """
+    Uploads a carousel (multiple images) to Instagram using the Graph API.
+    :param caption: The caption for the carousel post.
+    :param children_ids: List of IG container IDs (strings) for the carousel images.
+    """
+    url = f"https://graph.instagram.com/v22.0/{INSTAGRAM_ACCOUNT_ID}/media"
+    headers = {
+        "Content-Type": "application/json"
+    }
+    payload = {
+        "caption": caption,
+        "media_type": "CAROUSEL",
+        "children": ",".join(children_ids),
+        "access_token": INSTAGRAM_ACCESS_TOKEN
+    }
+    response = requests.post(url, headers=headers, json=payload)
+    print("Carousel Upload Response:", response.json())
+    return response.json()
+
+def publish_media_to_instagram(media_id):
+    # Publish the uploaded media with the media id given from single image upload.  this will take caption and image and post.
+    publish_url = f"https://graph.instagram.com/v22.0/{INSTAGRAM_ACCOUNT_ID}/media_publish"
+    publish_response = requests.post(
+        publish_url, 
+        data={
+            "creation_id": media_id, 
+            "access_token": INSTAGRAM_ACCESS_TOKEN
+        }
+    )
+
+    print("Publish Response:", publish_response.json())
 
 def get_all_instagram_posts():
     return database_service.get_all_instagram_posts()
--- services/twitter.py
+++ services/twitter.py
@@ -1,5 +1,8 @@
 import tweepy
 import os
+import requests
+from io import BytesIO
+from PIL import Image
 from services import database as database_service
 from dotenv import load_dotenv
 from models.twitter import TwitterPostCreate, TwitterPostUpdate
@@ -23,70 +26,91 @@ async def post_to_twitter(youtube_record: YouTubeTranscriptionCreate):
         access_token=ACCESS_TOKEN, access_token_secret=ACCESS_SECRET
     )
 
-    if youtube_record:
+    if youtube_record is not None:
         input_data = f"YouTube Video URL: https://www.youtube.com/watch?v={youtube_record.video_id} and transcription: {youtube_record.transcription}"
         agent_output = await twitter_service(input_data)
         
-        response = client.create_tweet(
-            text=agent_output.tweet
-        )
-        
-        tweet_text = response.data['text']
-        tweet_id = str(response.data['id'])
-        
-        print(f"Tweet text: {tweet_text}")
-        print(f"Tweet ID: {tweet_id}")
-        print(f"Youtube transcription ID: {youtube_record.id}")
-        
-        database_service.save_twitter_post(
-            youtube_transcription_id=youtube_record.id,
-            tweet=tweet_text,
-            tweet_id=tweet_id
-        )
-        
-        return {
-            "tweet_id": tweet_id,
-            "tweet": tweet_text,
-            "tweet_url": f"https://twitter.com/{tweet_obj.user.screen_name}/status/{tweet_id}"
-        }
-    
-    return {
-        "error": "No YouTube transcription found"
-    }
-
-async def post_tweet_service():
-    client = tweepy.Client(
-        consumer_key=API_KEY, consumer_secret=API_SECRET,
-        access_token=ACCESS_TOKEN, access_token_secret=ACCESS_SECRET
-    )
-    
-    latest_youtube_transcription = get_latest_youtube_transcription()
-    
-    if latest_youtube_transcription:
-        input_data = f"YouTube Video URL: https://www.youtube.com/watch?v={latest_youtube_transcription.video_id} and transcription: {latest_youtube_transcription.transcription}"
-        agent_output = await twitter_service(input_data)
+        return agent_output
         
         # response = client.create_tweet(
         #     text=agent_output.tweet
         # )
         
-        tweet_text = agent_output.tweet
+        # tweet_text = response.data['text']
         # tweet_id = str(response.data['id'])
-        tweet_id = "18989898989898989898"
         
-        print(f"Tweet text: {tweet_text}")
-        print(f"Tweet ID: {tweet_id}")
-        print(f"Youtube transcription ID: {latest_youtube_transcription.id}")
+        # print(f"Tweet text: {tweet_text}")
+        # print(f"Tweet ID: {tweet_id}")
+        # print(f"Youtube transcription ID: {youtube_record.id}")
         
-        database_service.save_twitter_post(
-            youtube_transcription_id=latest_youtube_transcription.id,
-            tweet=tweet_text,
-            tweet_id=tweet_id
-        )
+        # database_service.save_twitter_post(
+        #     youtube_transcription_id=youtube_record.id,
+        #     tweet=tweet_text,
+        #     tweet_id=tweet_id
+        # )
+        
         # return {
         #     "tweet_id": tweet_id,
         #     "tweet": tweet_text,
         #     "tweet_url": f"https://twitter.com/{tweet_obj.user.screen_name}/status/{tweet_id}"
+        # }
+    else:
+        pass
+        return {
+            "error": "No YouTube transcription found"
+        }
+
+async def post_tweet_service():
+    client = tweepy.Client(
+        consumer_key=os.getenv("TWITTER_API_KEY"), 
+        consumer_secret=os.getenv("TWITTER_API_SECRET"), 
+        access_token=os.getenv("TWITTER_ACCESS_TOKEN"), 
+        access_token_secret=os.getenv("TWITTER_ACCESS_SECRET")
+        # bearer_token=os.getenv("TWITTER_BEARER_TOKEN")
+    )
+
+    latest_youtube_transcription = get_latest_youtube_transcription()
+    
+    if latest_youtube_transcription:
+        input_data = f"""
+            YouTube Video URL: https://www.youtube.com/watch?v={latest_youtube_transcription.video_id} 
+            and transcription: {latest_youtube_transcription.transcription}
+        """
+        agent_output = await twitter_service(input_data)
+        
+        print(f"Agent output: {agent_output}")
+        
+        response = client.create_tweet(
+            text=agent_output.tweet
+        )
+        
+        main_tweet_id = response.data['id']
+        
+        # if agent_output.reply_tweets is not None:
+        #     for reply in agent_output.reply_tweets:
+        #         post_tweet_as_reply(reply.reply_tweet, main_tweet_id)
+        
+        # response = client.create_tweet(
+        #     text=agent_output.tweet
+        # )
+        
+        # tweet_text = agent_output.tweet
+        # tweet_id = str(response.data['id'])
+        
+        # print(f"Tweet text: {tweet_text}")
+        # print(f"Tweet ID: {tweet_id}")
+        # print(f"Youtube transcription ID: {latest_youtube_transcription.id}")
+        
+        # database_service.save_twitter_post(
+        #     youtube_transcription_id=latest_youtube_transcription.id,
+        #     tweet=tweet_text,
+        #     tweet_id=tweet_id
+        # )
+
+        # return {
+        #     "tweet_id": tweet_id,
+        #     "tweet": tweet_text,
+        #     "tweet_url": f"https://twitter.com/{response.data['username']}/status/{tweet_id}"
         # }
         return agent_output
     
@@ -94,6 +118,78 @@ async def post_tweet_service():
         "error": "No YouTube transcription found"
     }
     
+def upload_image_to_twitter(image_path: str, image_name: str):
+    client = tweepy.OAuth1UserHandler(
+        consumer_key=os.getenv("TWITTER_API_KEY"), 
+        consumer_secret=os.getenv("TWITTER_API_SECRET"), 
+        access_token=os.getenv("TWITTER_ACCESS_TOKEN"), 
+        access_token_secret=os.getenv("TWITTER_ACCESS_SECRET")
+    )
+
+    api = tweepy.API(client)
+
+    download_image(image_path, f"{image_name}.png")
+
+    img = Image.open(f"{image_name}.png")
+
+    # Save image in-memory
+    bytes_io = BytesIO()
+    img.save(bytes_io, "PNG")
+    bytes_io.seek(0)
+
+    # Upload the image
+    upload = api.media_upload(
+        filename=image_name,
+        file=bytes_io
+    )
+
+    print(f"Uploaded image: {upload}")
+    print(f"Uploaded image ID: {upload.media_id}")
+    
+    return upload.media_id
+
+def download_image(url, save_path):
+    response = requests.get(url)
+    if response.status_code == 200:
+        with open(save_path, 'wb') as f:
+            f.write(response.content)
+        print(f"Image downloaded and saved to {save_path}")
+    else:
+        print(f"Failed to download image. Status code: {response.status_code}")
+
+
+def post_tweet_as_reply(tweet: str, main_tweet_id: str, media_ids: list[str] = None):
+    """
+    Post a tweet to Twitter as a reply to the main tweet.
+    
+    Args:
+        tweet: The tweet to post
+        main_tweet_id: The ID of the main tweet to reply to
+    """
+    
+    client = tweepy.Client(
+        consumer_key=os.getenv("TWITTER_API_KEY"), 
+        consumer_secret=os.getenv("TWITTER_API_SECRET"), 
+        access_token=os.getenv("TWITTER_ACCESS_TOKEN"), 
+        access_token_secret=os.getenv("TWITTER_ACCESS_SECRET")
+    )
+    
+    response = client.create_tweet(
+        text=tweet,
+        in_reply_to_tweet_id=main_tweet_id,
+        media_ids=media_ids
+    )
+    
+    tweet_text = response.data['text']
+    username = response.data['username']
+    reply_tweet_id = str(response.data['id'])
+    
+    return {
+        "tweet_id": reply_tweet_id,
+        "tweet": tweet_text,
+        "tweet_url": f"https://twitter.com/{username}/status/{reply_tweet_id}"
+    }
+
 def get_latest_youtube_transcription():
     youtube_record = database_service.check_latest_transcription()
     
