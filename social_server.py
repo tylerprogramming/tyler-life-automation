744a847d7d99fbe7609005b1a26e10a85f783ae2
tylerreed <tylerreedytlearning@gmail.com>
2025-07-07 12:49:17 -0400

    removed unnecessary routing

--- social_server.py
+++ social_server.py
@@ -7,6 +7,10 @@ from routes.social_instagram import router as social_instagram_router
 from routes.social_twitter import router as social_twitter_router
 from routes.social_linkedin import router as social_linkedin_router
 from routes.social_youtube import router as social_youtube_router
+from routes.content import router as content_router
+from routes.dashboard import router as dashboard_router
+from routes.calendar import router as calendar_router
+from routes.google_calendar import router as google_calendar_router
 
 from psycopg2 import OperationalError
 
@@ -24,11 +28,15 @@ app.add_middleware(
 )
 
 # Include the social routes
+app.include_router(content_router, prefix="/content", tags=["Content"])
 app.include_router(social_router, tags=["Socials"])
 app.include_router(social_instagram_router, prefix="/instagram", tags=["Instagram"])
 app.include_router(social_twitter_router, prefix="/twitter", tags=["Twitter"])
 app.include_router(social_linkedin_router, prefix="/linkedin", tags=["LinkedIn"])
 app.include_router(social_youtube_router, prefix="/youtube", tags=["YouTube"])
+app.include_router(dashboard_router, prefix="/dashboard", tags=["Dashboard"])
+app.include_router(calendar_router, prefix="/calendar", tags=["Calendar"])
+app.include_router(google_calendar_router, prefix="/google-calendar", tags=["Google Calendar"])
 
 MAX_RETRIES = 10
 for i in range(MAX_RETRIES):
